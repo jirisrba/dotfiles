d2c6ffc74e1939b939a21a9e049fc70b0c3c287a
Jiri Srba <jiri.srba@gmail.com>
2020-08-21 15:08:49 +0200

    add aliase git pull

--- functions/update.fish
+++ functions/update.fish
@@ -15,9 +15,9 @@ function update
     pip3 install --upgrade pip setuptools wheel
     pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip3 install -U
     echo npm update
-    npm install npm -g; npm update -g
-    echo gem update
-    sudo gem update --system -n /usr/local/bin/gem && \
-      sudo -H gem update -n /usr/local/bin && \
-      sudo -H gem cleanup
+    npm install npm -g && npm update -g
+    #echo gem update
+    #sudo gem update --system -n /usr/local/bin/gem && \
+    #  sudo -H gem update -n /usr/local/bin && \
+    #  sudo -H gem cleanup
 end
