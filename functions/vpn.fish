e46b064141f98515efa3cbdc029fcecc6410b128
Jiri Srba <jiri.srba@gmail.com>
2020-11-03 17:17:33 +0100

    update vpn script

--- functions/vpn.fish
+++ functions/vpn.fish
@@ -1,18 +1,15 @@
 ## call AnyConnect
 function vpn
-  set vpn_connect "https://vpn.oict.cz:8080"
-  # set vpn_connect "vpn.csas.cz/linuxadm"  # mgmt vlan
+  set vpn_server "https://vpn.oict.cz:8080"
+  set username "OP3346"
+  set group "oict_internal"
 
   # kill running window process if exists
-  killall -d "Cisco AnyConnect Secure Mobility Client" &>/dev/null && \
-    killall "Cisco AnyConnect Secure Mobility Client"
+  # killall -d "Cisco AnyConnect Secure Mobility Client" &>/dev/null && \
+  #   killall "Cisco AnyConnect Secure Mobility Client"
 
   # cisco anyconnect connect to vpn
-  echo "vpn_connect: $vpn_connect"
-  /opt/cisco/anyconnect/bin/vpn -s connect $vpn_connect || return $status
+  echo "vpn_server: $vpn_server"
+  sudo openconnect-keychain --user=$username --authgroup=$group $vpn_server
   /sbin/ifconfig utun1 | grep "inet "
-
-  # renew kerberos ticket
-  # sleep 2
-  # kinit --keychain sol60210@CEN.CSIN.CZ && klist || return $status
 end
